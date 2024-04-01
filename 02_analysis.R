6823ee3e91193719dd679e7caafe15da450ac9e7
henry-kanengiser <49458585+henry-kanengiser@users.noreply.github.com>
2024-04-01 14:21:58 -0400

    add code to export a joined solar shapefile for mapmaking

--- 02_analysis.R
+++ 02_analysis.R
@@ -57,8 +57,26 @@ depot_solar_sum <- depot_solar %>%
   ungroup()
 
 
+# 4. Save shapefile with complete information ---------------------------------
+# first, combine solar files (geometry is no longer required, so drop it now)
+solar_shp <- bind_rows(solar_bldg, solar_prkg) %>% 
+  # st_drop_geometry() %>%
+  mutate(type = ifelse(is.na(source_id), "building", "parking lot"),
+         bldg = type == "building",
+         prkg = type == "parking lot")
 
-# 4. Write to permanent file
+# join with the bblkey file to generate depot name info
+depot_solar_shp <- bblkey %>%
+  select(borough, depot, bbl) %>%
+  full_join(solar_shp, by = "bbl")
+
+
+
+# 5. Write to permanent file --------------------------------------------------
 
 write_csv(depot_solar_sum, "dat/OUT/depot_solar_summary.csv")
+
+# save shapefile for mapping
+st_write(depot_solar_shp, "dat/out/depot_solar.shp")
+
             
